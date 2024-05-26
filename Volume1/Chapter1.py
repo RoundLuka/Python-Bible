908cd01013185b258f7bc04f61970d9f66a29626
RoundLuka <lukagurgenidze282007@gmail.com>
2024-05-26 21:42:14 +0400

    first volume

---
+++ Volume1/Chapter1.py
@@ -0,0 +1,18 @@
+# print("Hello World!")
+
+# myNum = 10
+# myText = "luka"
+
+# myNum += 5
+# print(myNum)
+
+# name_ages = {
+#     "Luka" : 16,
+#     "Gio"  : 4
+# }
+
+# file = open ( 'myfile.txt' , 'w' )
+# print (file.write( "Hello File!" ))
+# file.flush()
+# file.close()
+
